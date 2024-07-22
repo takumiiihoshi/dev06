66356714d131606684ae31c2bb4009a1776800ba
takumiiihoshi <TAKUMI.IIHOSHI@grp.tmnf.jp>
2024-07-22 13:21:29 +0900

    helloのテストコード作成

--- devUnitApp/unitApp/tests.py
+++ devUnitApp/unitApp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class IndexViewTests(TestCase):
+    def test_index_view(self):
+        response = self.client.get(reverse('unitApp:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<h1>Hello</h1>')
