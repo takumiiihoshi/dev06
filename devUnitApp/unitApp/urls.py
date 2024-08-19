67755331d209091a5ca4fe101bb0f7216a932232
takumiiihoshi <TAKUMI.IIHOSHI@grp.tmnf.jp>
2024-08-19 15:07:35 +0900

    [WIP]addTaskAPI呼び出し（エラー出てる）

--- devUnitApp/unitApp/urls.py
+++ devUnitApp/unitApp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from .views import TodoViewSet
+from .views import TodoViewSet, AddTaskView
 
 from . import views
 
@@ -9,6 +9,7 @@ router = DefaultRouter()
 router.register(r'todos', TodoViewSet)
 
 urlpatterns = [
-    path('hello', views.index, name='index'),
-    path('', include(router.urls)),  # APIルートの追加
+    path('', views.index, name='index'),
+    path('', include(router.urls)),
+    path('addTask/', AddTaskView.as_view(), name='add_task'),
 ]
--- devUnitApp/unitApp/views.py
+++ devUnitApp/unitApp/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import viewsets
 from .models import Todo
 from .serializers import TodoSerializer
 
 
 # Create your views here.
-
+# ToDoアイテムのリストを取得
 def index(request):
     return render(request, 'index.html')
+
+# ToDoアイテムを追加
+class AddTaskView(APIView):
+    def post(self, request):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
