78a10626ac19cfc92194f37416f25d59256305c2
hyunwooshin <sisn0596@gmail.com>
2024-09-10 09:44:54 +0900

    feat: Todo削除機能実装

--- devUnitApp/unitApp/urls.py
+++ devUnitApp/unitApp/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from .views import TodoViewSet
-
 from . import views
-
 app_name = 'unitApp'
 router = DefaultRouter()
 router.register(r'todos', TodoViewSet)
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('', include(router.urls)),
-    path('todos/addTask/', TodoViewSet.as_view({'post': 'add_task'}), name='add_task')
+    path('todos/addTask/', TodoViewSet.as_view({'post': 'add_task'}), name='add_task'),
+    path('todos/deleteTask/', TodoViewSet.as_view({'post': 'delete_task'}), name='delete_task')
 ]
--- devUnitApp/unitApp/views.py
+++ devUnitApp/unitApp/views.py
@@ -7,17 +7,13 @@ from rest_framework import status
 from rest_framework import viewsets
 from .models import Todo
 from .serializers import TodoSerializer
-
-
 # Create your views here.
 # ToDoアイテムのリストを取得
 def index(request):
     return render(request, 'index.html')
-
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
-
     @action(detail=False, methods=['post'], url_path='addTask')
     def add_task(self, request):
         serializer = TodoSerializer(data=request.data)
@@ -25,3 +21,8 @@ class TodoViewSet(viewsets.ModelViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    @action(detail=False, methods=['delete'], url_path='deleteTask')
+    def delete_task(self, request):
+        todo = Todo.objects.get(pk=request.data['id'])
+        todo.delete()
+        return Response(status=status.HTTP_200_BAD_REQUEST)
